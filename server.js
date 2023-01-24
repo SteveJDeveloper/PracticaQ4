ee312c5659a286e05daa9ba39f91aa8d7cc344f1
SteveJDeveloper <103290510+SteveJDeveloper@users.noreply.github.com>
2023-01-24 16:46:15 -0500

    Json server with auth

--- server.js
+++ server.js
@@ -1,43 +1,41 @@
-// server.js
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
 const server = jsonServer.create()
 const path = require('path')
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-const validatorsPost = ['author','title']
+const validatorsPost = ['author', 'title']
 
 server.use(middlewares)
-
-server.use((req, res, next) => {
-    if (req.headers.authorization === "hola") {
-        next()
-    } else {
-        res.status(401).jsonp({
-            error: "Afuera de aqui"
-        })
-    }
-})
 
 server.use(jsonServer.bodyParser)
 
 server.post('/posts', (req, res, next) => {
     let menssage = ""
     validatorsPost.forEach(validator => {
-        if(!req.body.hasOwnProperty(validator)){
+        if (!req.body.hasOwnProperty(validator)) {
             menssage += " " + validator
         }
     });
-    if (menssage === ""){
+    if (menssage === "") {
         next()
-    }else{
+    } else {
         res.status(206).jsonp({
-            error: "Falta propiedades:"+menssage
+            error: "Falta propiedades:" + menssage
         })
     }
-        
+
 })
 
+const rules = auth.rewriter({
+    // Permission rules
+    posts: 660,
+})
+
+server.use(rules)
+server.db = router.db
+server.use(auth)
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
